35a797b2561cdc9bae06846e0f324e2add11d03c
Ruhiddin <niddihur@gmail.com>
2025-04-20 05:06:01 +0500

    fix: gitignore: .github folder added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redisimnest',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'redis>=5.0', 
